5279dc92fe64050f12751dd0d020c81a03aa6035
changkm <changkinman06@163.com>
2026-05-07 16:31:54 +0800

    完成day09代码（配送范围相关功能未实现）

--- sky-common/src/main/java/com/sky/exception/OrderBusinessException.java
+++ sky-common/src/main/java/com/sky/exception/OrderBusinessException.java
@@ -6,4 +6,5 @@ public class OrderBusinessException extends BaseException {
         super(msg);
     }
 
+
 }
--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -42,4 +42,7 @@ public interface ShoppingCartMapper {
      */
     @Delete("delete from shopping_cart where user_id = #{userId}")
     void deleteByUserId(Long userId);
+
+
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
